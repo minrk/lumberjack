f2f1ec3bc0a3ceba379a5290291b3eac3e70057c
MinRK <benjaminrk@gmail.com>
2012-04-20 19:04:03 -0700

    support queries in JSONHandler

--- web/server.py
+++ web/server.py
@@ -11,6 +11,8 @@ import tornado.web
 
 from tornado.options import define, options
 
+from lumberjackdb import LumberJackDatabase
+
 define("port", default=8888, help="run on the given port", type=int)
 define("db", default="../bot/lumberjack.db", type=str)
 
@@ -21,24 +23,35 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class JSONHandler(tornado.web.RequestHandler):
     def get(self):
+        db = self.application.db
         n = self.get_argument('n', 50)
-        channel = self.get_argument('channel', 'ipython')
-        id = self.get_argument('id', '')
-        search = self.get_argument('search', '')
+        channel = self.get_argument('channel', '#ipython_bot')
+        id = self.get_argument('id', 0)
         offset = self.get_argument('offset', 0)
-        switch = self.get_argument('type', 'default')
-        self.write(json.dumps(['hello']))
-    
-    def connect_db(self):
-        for i in range(5):
-            try:
-                self.db = sqlite3.connect(self.settings.db)
-            except Exception:
-                time.sleep(0.2)
+        switch = self.get_argument('type', 'update')
+        search = self.get_arguments('search', [])
+        if search:
+            switch = "search"
+        if switch == 'tail':
+            result = db.get_last(channel, n)
+        elif switch == 'update':
+            result = db.get_last(channel, n)
+        elif switch == 'search':
+            result = db.search(channel, search, limit=n, offset=offset)
+        elif switch == 'context':
+            ctx = self.get_argument('context', 'both')
+            if ctx == 'before':
+                result = db.get_before(channel, id, n)
+            elif ctx == 'after':
+                result = db.get_after(channel, id, n)
             else:
-                return
-        raise tornado.web.HTTPError(500)
-        
+                result = db.get_context(channel, id, n)
+        else:
+            logging.error("unhandled request: %s" % switch)
+            import IPython
+            IPython.embed()
+            result = []
+        self.write(json.dumps(list(result)))
     
 
 def main(db_file):
@@ -48,9 +61,9 @@ def main(db_file):
         (r"/json", JSONHandler),
         ],
         static_path=os.path.join(os.path.dirname(__file__), "static"),
-        db_file = db_file,
-        
     )
+    application.db = LumberJackDatabase(db_file)
+    
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
@@ -58,5 +71,7 @@ def main(db_file):
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        db_file = sys.argv
-    main()
+        db_file = sys.argv[1]
+    else:
+        db_file = '../bot/lumberjack.db'
+    main(db_file)
